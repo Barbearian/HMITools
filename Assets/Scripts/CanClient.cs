1774631f1f7fc5eb420cba6f0c99d2ac5f9a1d30
Barbearian <wouyang2@outlook.com>
2023-02-13 20:30:37 +0800

    added version

--- Assets/Scripts/CanClient.cs
+++ Assets/Scripts/CanClient.cs
@@ -9,24 +9,69 @@ namespace Bear
     {
         public string url;
         public int port;
+        public bool ConnectOnAwake;
+        private Socket socket;
+        private static CanClient _instance;
+        public static CanClient Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameObject("CanClient").AddComponent<CanClient>();
+                    DontDestroyOnLoad(_instance);
+
+                }
+                return _instance;
+
+            }
+        }
+
+        private void Awake()
+        { 
+            if (ConnectOnAwake)
+                Connect();
+        }
+
         public void Connect()
         {
+            if (socket != null) {
+                Disconnect();
+            }
+
             // Set up the endpoint for the remote device.
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(url), port);
 
-            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            sender.Connect(remoteEndPoint);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(remoteEndPoint);
 
+            
+
+        }
+
+        public void Connect(string url, int port)
+        {
+            this.url = url;
+            this.port = port;
+            Connect();
+        }
+
+        public void SendData(byte[] data){
+            socket.Send(data);
         }
 
         public void Disconnect()
         {
+            Disconnect(socket);
+        }
 
+        private void Disconnect(Socket socket) {
+            socket?.Shutdown(SocketShutdown.Both);
+            socket?.Close();
         }
 
         public void OnDestroy()
         {
-            
+            Disconnect();
         }
     }
 }
---
+++ Assets/Scripts/CanClientTestor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using IngameDebugConsole;
+namespace Bear
+{
+    public class CanClientTestor : MonoBehaviour
+    {
+        [ConsoleMethod("Connect","Try to Connect with url and ip")]
+        public static void Connect(string url,int port)
+        {
+            CanClient.Instance.Connect(url,port);
+        }
+
+        [ConsoleMethod("SendData", "Try to Connect with url and ip")]
+        public static void SendData(byte[] data)
+        {
+            CanClient.Instance.SendData(data);
+            //Debug.Log(data[0]);
+        }
+    }
+}
